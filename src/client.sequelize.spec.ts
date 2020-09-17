e98ba56071dd1130fd161824d1a67fe8a953885a
Roger Jaggi <roger@bbit.io>
2020-09-17 17:00:58 +0200

    ci(actions): fixing timeout
    {"contact":"#itbrawret2blf9mxdxl6","project":"#cMSagM6wAiyAURCX6rCpp","milestone":"#ByrBVPemZkWxWUFQt7aCr","type":"#pUZm65spF5eVn57DZ4Ayz","time":0.38,"billable":true}

--- src/client.sequelize.spec.ts
+++ src/client.sequelize.spec.ts
@@ -6,60 +6,64 @@ const dbUrl = process.env.AURORA_TEST_DB_URL;
 console.log(dbUrl);
 
 describe('Simulate raw postgres client', () => {
-  test('create table, insert and retrieve a record', async () => {
-    const client = new lib.Client(dbUrl);
-    const options = client.dataApiRetrievePostgresDataApiClientConfig();
-    console.log(options);
+  test(
+    'create table, insert and retrieve a record',
+    async () => {
+      const client = new lib.Client(dbUrl);
+      const options = client.dataApiRetrievePostgresDataApiClientConfig();
+      console.log(options);
 
-    const sequelize = new Sequelize({
-      ...(options as any),
-      dialect: 'postgres',
-      dialectModule: lib,
-    });
+      const sequelize = new Sequelize({
+        ...(options as any),
+        dialect: 'postgres',
+        dialectModule: lib,
+      });
 
-    await sequelize.authenticate();
+      await sequelize.authenticate();
 
-    class User extends Model {
-      public id!: number; // Note that the `null assertion` `!` is required in strict mode.
-      public name!: string;
-      public age!: number | null; // for nullable fields
-    }
+      class User extends Model {
+        public id!: number; // Note that the `null assertion` `!` is required in strict mode.
+        public name!: string;
+        public age!: number | null; // for nullable fields
+      }
 
-    User.init(
-      {
-        id: {
-          type: DataTypes.INTEGER.UNSIGNED,
-          autoIncrement: true,
-          primaryKey: true,
+      User.init(
+        {
+          id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+          },
+          name: {
+            type: new DataTypes.STRING(128),
+            allowNull: false,
+          },
+          age: {
+            type: new DataTypes.INTEGER(),
+            allowNull: true,
+          },
         },
-        name: {
-          type: new DataTypes.STRING(128),
-          allowNull: false,
+        {
+          tableName: 'users',
+          sequelize, // passing the `sequelize` instance is required
         },
-        age: {
-          type: new DataTypes.INTEGER(),
-          allowNull: true,
-        },
-      },
-      {
-        tableName: 'users',
-        sequelize, // passing the `sequelize` instance is required
-      },
-    );
+      );
 
-    await sequelize.sync({ alter: true });
+      await sequelize.sync({ alter: true });
 
-    const newUser = await User.create({
-      name: 'Johnny',
-      age: 30,
-    });
-    console.log(newUser.id, newUser.name, newUser.age);
+      const newUser = await User.create({
+        name: 'Johnny',
+        age: 30,
+      });
+      console.log(newUser.id, newUser.name, newUser.age);
 
-    const foundUser = await User.findOne({ where: { name: 'Johnny' } });
-    expect(foundUser).toBeTruthy();
-    expect(foundUser.name).toBe('Johnny');
-    expect(foundUser.id).toBeGreaterThan(0);
+      const foundUser = await User.findOne({ where: { name: 'Johnny' } });
+      expect(foundUser).toBeTruthy();
+      expect(foundUser.name).toBe('Johnny');
+      expect(foundUser.id).toBeGreaterThan(0);
 
-    await sequelize.close();
-  });
+      await sequelize.close();
+    },
+    15 * 1000,
+  );
 });
